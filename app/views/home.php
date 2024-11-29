3fa9efc137bc8c4d78fd6e3d04f14f5ab284ff17
SkyKipz <aaron.moralesva@alumno.buap.mx>
2024-11-28 22:54:05 -0600

    Eliminación de enlace del formulario
    Se eliminó la redirección del enlace al formulario para ahora tenerlo en el botón adecuado.

--- app/views/home.php
+++ app/views/home.php
@@ -1,5 +1,5 @@
 <?php
-echo '<a href="/MANA/public/layout/pages/form.html">Fomurlario</a>';
+//echo '<a href="/MANA/public/layout/pages/form.html">Formulario</a>';
 require_once __DIR__ . '/../../public/index.html';
 
 ?>  
