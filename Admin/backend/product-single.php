2211b7b69894b85cdb29501f3e0044f0d3fc5022
N0BL3R <nomar.ledo@alumno.buap.mx>
2024-12-02 00:57:41 -0600

    Revert "Pagina de Administrador"
    This reverts commit 88bbada32a9da00fcdca96ee791192519cab5feb.

--- Admin/backend/product-single.php
+++
@@ -1,8 +0,0 @@
-<?php
-    use TECWEB\MYAPI\Products;
-    require_once __DIR__.'/myapi/Products.php';
-
-    $productos = new Products('mana');
-    $productos->single( $_POST['id'] );
-    echo $productos->getData();
-?>
