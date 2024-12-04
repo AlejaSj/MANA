d200b525bfa29204f0ddcb0a8b630f3f3983f881
Miriam <miriam.sanchezga@alumno.buap.mx>
2024-12-04 16:48:01 -0600

    carpetas formulario

--- app/views/home.php
+++ app/views/home.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page - Cuidado del Medio Ambiente</title>
-    <link rel="stylesheet" href="/public/css/styles.css">
+    <link rel="stylesheet" href="./public/css/styles.css">
 </head>
 <body>
     <header class="hero">
-        <h1>Protejamos el Planeta</h1>
+        <h1>Protejamos el Planseta</h1>
         <button id="getTips">Obtener Consejos</button>
     </header>
     <section id="tipsContainer"></section>
-    <script src="/public/js/scripts.js"></script>
+    <script src="./public/js/scripts.js"></script>
 </body>
 </html>
