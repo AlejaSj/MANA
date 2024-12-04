30f0ebc9f93261ecaee44da27669ffeb81eaf49f
Miriam <miriam.sanchezga@alumno.buap.mx>
2024-12-04 17:00:56 -0600

    formato AboutUs.php

--- app/views/AboutUs.php
+++ app/views/AboutUs.php
@@ -8,77 +8,50 @@
 <link rel="icon" href="/MANA/public/images/favicon-16x16.png" type="image/x-icon">
 </head>
 <body id="AboutUs">
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
+
 <div class="wrapper row1">
   <header id="header" class="hoc clear">
     <div id="logo" class="fl_left"> 
-      <!-- ################################################################################################ -->
+    
       <h1 class="logoname"><a href="/MANA/app/views/home.php"><span>M</span><span>A</span><span>N</span><span>A</span></a></h1>
-      <!-- ################################################################################################ -->
+     
     </div>
     <nav id="mainav" class="fl_right"> 
-      <!-- ################################################################################################ -->
+      
       <ul class="clear">
         <li class="active"><a href="/mana/">Inicio</a></li>
-        <!-- <li><a class="drop" href="#">Pages</a>
-          <ul>
-            <li><a href="/MANA/public/layout/pages/gallery.html">Gallery</a></li>
-            <li><a href="/MANA/public/layout/pages/full-width.html">Full Width</a></li>
-            <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
-            <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
-            <li><a href="pages/basic-grid.html">Basic Grid</a></li>
-            <li><a href="pages/font-icons.html">Font Icons</a></li>
-          </ul>
-        </li>
-        <li><a class="drop" href="#">Dropdown</a>
-          <ul>
-            <li><a href="#">Level 2</a></li>
-            <li><a class="drop" href="#">Level 2 + Drop</a>
-              <ul>
-                <li><a href="#">Level 3</a></li>
-                <li><a href="#">Level 3</a></li>
-                <li><a href="#">Level 3</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Level 2</a></li>
-          </ul>
-        </li> -->
+        
         <li><a href="http://localhost/MANA/app/views/AboutUs.php">Acerca de nosotros</a></li>
         <li><a href="/mana/#ContactUs">Contáctanos</a></li> 
         <li><a href="/mana/#Objective">Objetivos</a></li>
         <li><a href="https://github.com/AlejaSj/MANA/">Repositorio</a></li>
         <li><a href="http://localhost/MANA/app/views/dashboard.php">Estadísticas</a></li>
       </ul>
-      <!-- ################################################################################################ -->
+      
     </nav>
   </header>
 </div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
+
 <div class="bgded overlay" style="background-image:url('/MANA/public/images/landing/backgrounds/01.png');">
   <div id="pageintro" class="hoc clear"> 
-    <!-- ################################################################################################ -->
+    
     <article>
       <h3 class="heading">Acerca de Nosotros</h3>
       <p style="font-size: 20px;">Somos un grupo pequeño de estudiantes que quiere informar a la comunidad acerca de lo importante que es la conservación de la vida de los ecosistemas terrestres.</p>
     </article>
-    <!-- ################################################################################################ -->
+    
   </div>
 </div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
+
 
 <a id="backtotop" href="#AboutUs"><i class="fas fa-chevron-up"></i></a>
-<!-- JAVASCRIPTS -->
+
+
 <script src="/MANA/public/layout/scripts/landing/jquery.min.js"></script>
 <script src="/MANA/public/layout/scripts/landing/jquery.backtotop.js"></script>
 <script src="/MANA/public/layout/scripts/landing/jquery.mobilemenu.js"></script>
-<!-- Homepage specific -->
+
 <script src="/MANA/public/layout/scripts/landing/jquery.easypiechart.min.js"></script>
-<!-- / Homepage specific -->
+
 </body>
 </html>
